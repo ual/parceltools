7d5fbe41a7f50bf86f381e72e399b0f1af61e7ca
Brian Cavagnolo <bcavagnolo@berkeley.edu>
2013-03-31 13:32:41 -0700

    resolve duplicate multipolygon relations
    Specifically:
    
    -- Factor out code that consolidates tags
    -- Resolve duplicate ways and duplicate relations
    -- Assert if we see something we don't know about

--- tests/sf.t
+++ tests/sf.t
@@ -29,7 +29,43 @@ overlapways:
   Merging points
   Making list
   Checking list
-  Making list
   Resolving 5 duplicate ways
   Outputting XML
   $ xmllint --format overlapways.osm | diff -uNr - $TESTDIR/overlapways-ref.osm
+
+multiparcel:
+  $ ogr2osm.py -f $TESTDIR/multiparcel.shp -t sf
+  running with lxml.etree
+  Preparing to convert file .* (re)
+  Will try to detect projection from source metadata, or fall back to EPSG:4326
+  Successfully loaded 'sf' translation method .* (re)
+  Using default filterLayer
+  Using default filterFeature
+  Using user filterTags
+  Using default filterFeaturePost
+  Using user preOutputTransform
+  Parsing data
+  Detected projection metadata:
+  PROJCS["NAD_1983_StatePlane_California_III_FIPS_0403_Feet",
+      GEOGCS["GCS_North_American_1983",
+          DATUM["North_American_Datum_1983",
+              SPHEROID["GRS_1980",6378137.0,298.257222101]],
+          PRIMEM["Greenwich",0.0],
+          UNIT["Degree",0.017453292519943295]],
+      PROJECTION["Lambert_Conformal_Conic_2SP"],
+      PARAMETER["False_Easting",6561666.666666666],
+      PARAMETER["False_Northing",1640416.666666667],
+      PARAMETER["Central_Meridian",-120.5],
+      PARAMETER["Standard_Parallel_1",37.06666666666667],
+      PARAMETER["Standard_Parallel_2",38.43333333333333],
+      PARAMETER["Latitude_Of_Origin",36.5],
+      UNIT["Foot_US",0.30480060960121924],
+      PARAMETER["scale_factor",1.0]]
+  Merging points
+  Making list
+  Checking list
+  Resolving 4 duplicate ways
+  Resolving 4 duplicate ways
+  Resolving 4 duplicate relations
+  Outputting XML
+  $ xmllint --format multiparcel.osm | diff -uNr - $TESTDIR/multiparcel-ref.osm
--- translations/sf.py
+++ translations/sf.py
@@ -14,18 +14,27 @@ def filterTags(attrs):
 # wrapper class for Ways.  I did this instead of overriding the base class'
 # __hash__ and __eq__ functions because in other places we need the default
 # versions.
-class ComparableWay():
-    def __init__(self, way):
-        self.way = way
+class ComparableGeometry():
+
+    def __init__(self, geometry):
+        self.geometry = geometry
+
+class ComparableWay(ComparableGeometry):
 
     def __hash__(self):
-        return hash(tuple(self.way.points))
+        return hash(tuple(self.geometry.points))
 
     def __eq__(self, other):
-        return (self.way.points == other.way.points)
+        return (self.geometry.points == other.geometry.points)
+
+class ComparableRelation(ComparableGeometry):
+    def __hash__(self):
+        return hash(tuple(self.geometry.members))
+
+    def __eq__(self, other):
+        return (self.geometry.members == other.geometry.members)
 
 def getDuplicates(items):
-    l.debug("Making list")
     dups = {}
     for i in items:
         if i in dups:
@@ -37,28 +46,60 @@ def getDuplicates(items):
 def getParent(w):
     return iter(w.parents).next()
 
+def getGrandParent(w):
+    return getParent(getParent(w))
+
+def consolidateTags(dups, features):
+    ordered = sorted(dups, key=lambda d: getParent(d.geometry).tags['apn'])
+    final_tags = {}
+    index = 0
+    for r in ordered:
+        f = getParent(r.geometry)
+        final_tags['apn:%d' % (index)] = f.tags['apn']
+        if index != 0:
+            features.remove(f)
+            f.geometry.removeparent(f)
+        index += 1
+
+    tags = getParent(ordered[0].geometry).tags
+    del tags['apn']
+    tags.update(final_tags)
+
 # We handle two sorts of duplicate Ways here.  One is a set of stand-alone Ways
 # with parent Features that have some tags.  The other case is a set of Ways
 # that have multipolygon Relation parents and Feature grandparents with tags.
 def preOutputTransform(geometries, features):
-    ways = [ComparableWay(w) for w in geometries if type(w) == ogr2osm.Way and \
-            type(getParent(w)) == ogr2osm.Feature]
+    ways = [ComparableWay(w) for w in geometries if type(w) == ogr2osm.Way]
+
     for (way, dups) in getDuplicates(ways).items():
         if len(dups) == 1:
             continue
-        l.info("Resolving " + str(len(dups)) + " duplicate ways")
-        final = None
-        ordered = sorted(ways, key=lambda w: getParent(w.way).tags['apn'])
-        final_tags = {}
-        index = 0
-        for w in ordered:
-            p = getParent(w.way)
-            final_tags['apn:%d' % (index)] = p.tags['apn']
-            if index != 0:
-                features.remove(p)
-                p.geometry.removeparent(getParent(w.way))
-            index += 1
 
-        tags = getParent(ordered[0].way).tags
-        del tags['apn']
-        tags.update(final_tags)
+        l.info("Resolving " + str(len(dups)) + " duplicate ways")
+
+        if type(getParent(way.geometry)) == ogr2osm.Relation and \
+               len(way.geometry.parents) == 1 and \
+               type(getGrandParent(way.geometry)) == ogr2osm.Feature:
+            # If the duplicate ways belong to Relations, we assume that they
+            # are pieces of multipolygons and that all of the attributes are
+            # stored with the Relation.
+            for w in dups[1:]:
+                for p in set(w.geometry.parents):
+                    p.replacejwithi(dups[0].geometry, w.geometry)
+
+        elif type(getParent(way.geometry)) == ogr2osm.Feature and \
+                 len(way.geometry.parents) == 1:
+            consolidateTags(dups, features)
+
+        else:
+            # To be sure we catch anything down the line that does not fit into
+            # our two simple cases, assert.
+            l.error("Unsupported geometry arrangement")
+            assert(False)
+
+    relations = [ComparableRelation(r) for r in geometries if type(r) == ogr2osm.Relation]
+    for (relation, dups) in getDuplicates(relations).items():
+        if len(dups) == 1:
+            continue
+        l.info("Resolving " + str(len(dups)) + " duplicate relations")
+        consolidateTags(dups, features)
