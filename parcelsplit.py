61be3a7b750412c9e8f41f420692f950438a8194
Brian Cavagnolo <bcavagnolo@berkeley.edu>
2013-04-16 16:38:06 -0700

    add an option to select a block prefix from an assessor parcel number
    This actually may not work if the APN field from which the block
    number is extracted has NULLs that are part of the same block as
    parcels that don't have NULLs.

--- parcelsplit.py
+++ parcelsplit.py
@@ -41,21 +41,55 @@ def create_dest(outdir, i, layer):
     return ds
 
 def divide_by_blocknum(layer, options):
+
+    def get_blocknum(f):
+
+        if not f.GetField(options.blocknum):
+            return None
+
+        if options.prefix_len:
+            return eval('f.GetField(options.blocknum)[0:%d]' % options.prefix_len)
+
+        return f.GetField(options.blocknum)
+
     if options.index:
         l.info('Creating index...')
         src.ExecuteSQL('CREATE INDEX ON ' + layer.GetName() + ' USING ' + options.blocknum)
 
-    l.info('Reading features...')
+    total = layer.GetFeatureCount()
+    l.info('Reading %d features...' % total)
     blocknums = set()
-    features = [layer.GetNextFeature().GetField(options.blocknum) for i in range(layer.GetFeatureCount())]
+    features = [layer.GetNextFeature() for i in range(layer.GetFeatureCount())]
+
+    # If a prefix length was specified, we need to know how long the suffix is.
+    fill = None
+    if options.prefix_len:
+        full_blocknums = [len(f.GetField(options.blocknum)) for f in features if f.GetField(options.blocknum)]
+        min_len = min(full_blocknums)
+        max_len = min(full_blocknums)
+        if min_len != max_len:
+            l.warning("IDs vary from %d to %d and I (wrongly) assume that they are all %d" % \
+                      (min_len, max_len, min_len))
+        fill = min_len - options.prefix_len
+    features = [get_blocknum(f) for f in features]
     blocknums = sorted([x for x in features if x not in blocknums and not blocknums.add(x)])
+
+    def get_query(b):
+        if options.prefix_len:
+            return options.blocknum + ' IS NOT NULL AND ' + \
+                   options.blocknum + ' >= ' + b + fill*'0' + ' AND ' + \
+                   options.blocknum + ' <= ' + b + fill*'9'
+        return options.blocknum + '=' + b
 
     dest = create_dest(options.outdir, 0, layer)
     dlayer = dest.GetLayerByIndex(0)
     i = 0
+    count = 0
     for b in blocknums:
-        layer.SetAttributeFilter(options.blocknum + '=' + b)
-
+        if b:
+            layer.SetAttributeFilter(get_query(b))
+        else:
+            layer.SetAttributeFilter(options.blocknum + ' IS NULL')
         if dlayer.GetFeatureCount() > 0 and \
            (layer.GetFeatureCount() + dlayer.GetFeatureCount() > options.max_features or \
             layer.GetFeatureCount() > options.max_features):
@@ -70,6 +104,7 @@ def divide_by_blocknum(layer, options):
                       ' features but the max is ' + str(options.max_features))
 
         [dlayer.CreateFeature(layer.GetNextFeature()) for f in range(layer.GetFeatureCount())]
+        count += layer.GetFeatureCount()
 
     if dlayer.GetFeatureCount() == 0:
         n = dest.name
@@ -78,6 +113,9 @@ def divide_by_blocknum(layer, options):
     else:
         l.info('Wrote ' + str(dlayer.GetFeatureCount()) + ' to ' + dest.name)
         dest.Destroy()
+
+    if count != total:
+        l.warning("Read %d total features but wrote %d." % (total, count))
 
 def _sort_one_pass(features):
     sorted = {}
@@ -163,13 +201,18 @@ if __name__ == "__main__":
     parser.add_option('-l', '--layer', dest='layer',
                       help='''Layer in the shpfile to split.''')
     parser.add_option('-b', '--block-number-attribute', dest='blocknum',
-                      help='''Attribute that represents the block number''')
+                      help='''Attribute that contains the block number''')
     parser.add_option('-i', '--index', dest='index', action="store_true",
                       help='''Create an index to speed up processing''')
     parser.add_option('-m', '--max-features', dest='max_features', default=1000,
                       help='''Maximum features per chunk (Defaults to 1000)''', type=int)
     parser.add_option('-o', '--output-directory', dest='outdir', default='.',
                       help='''Output directory for chunks''')
+    parser.add_option('-p', '--prefix-length', dest='prefix_len', type=int,
+                      help='''Length of the block number.  If this is unspecified,
+                              The entire attribute specified by -b is assumed to be
+                              the block number.  Otherwise, the attribute is truncated
+                              to the length specified by -p.''')
 
     (options, args) = parser.parse_args()
     if len(args) != 1:
